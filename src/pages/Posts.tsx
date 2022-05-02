1f04f987e1e6e50b7e3ab89d155fbf48fae2a5d0
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-05-02 11:04:55 +0200

    feat(pages) : récupération de data avec fetch

--- src/pages/Posts.tsx
+++ src/pages/Posts.tsx
@@ -1,7 +1,20 @@
-import PostsList from '../components/PostsList'
-import './posts.scss'
+import { useEffect, useState } from 'react';
+import PostsList from '../components/PostsList';
+import './posts.scss';
 
-const Posts = () => {
+const Posts: React.FC = () => {
+  const [allPosts, setAllPosts] = useState(null);
+
+  useEffect(() => {
+    const getPost = async() => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=');
+      const data = await response.json();
+      setAllPosts(data);
+    }
+    getPost();
+
+  }, [])
+
   return (
     <div className="post-container">
       <h1>Page principale</h1>
